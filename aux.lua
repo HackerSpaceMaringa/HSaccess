659ca3b32bdfd9a615b8cb614047c0b0fa46291b
jcert <jgcertorio@uol.com.br>
2018-01-08 22:25:43 -0200

    long function definitions are now here

---
+++ aux.lua
@@ -0,0 +1,28 @@
+function listap(t)
+    for k,v in pairs(t) do
+        print(k.." : "..v)
+    end
+end
+
+function DBsearch(user_name,dbFile)
+	file.open(dbFile) -- check if file exists
+	found = false
+	ended = false
+	while ((not found)and(not ended) ) do
+	  local result = ""
+	  local last = ""
+	  while(string.sub(last,#last,#last) ~= "\n" and last ~= nil) do
+	    last = file.readline()
+	    result = result .. last
+	  end
+		if result == nil then
+			ended = true
+			break
+		end
+		user,salt,hash = string.match(result,"(.*),(.*),(.*)\n")
+		if user == user_name then
+			return {user=user,salt=salt,hash=hash}
+		end
+	end
+end
+
