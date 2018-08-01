96422b387ddc39dcc768473c6b566f7ee4af4e0a
jcert <jgcertorio@uol.com.br>
2018-07-31 22:55:29 -0300

    reduzi espaços no html. dividi o init em vários arquivos, compilando os que estão estáveis.mudei o upload para baixar o arquivo de log

--- aux.lua
+++ aux.lua
@@ -1,7 +1,24 @@
+pin = 0;
+pl  = 12000;
+gpio.mode(pin, gpio.OUTPUT);
+servo = {min=500,max=2400}
+
+done_page=[[<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+</head>
+<link rel="icon" href="data:;base64,iVBORw0KGgo=">
+<body>
+  <title>Acesso - HackerSpace Maringá</title>
+  <meta name="description" content="Access page">
+  <meta name="author" content="HS Maringa">
+  <H1>HackerSpace Maringá</H1><br>]]
+
 function listap(t)
-    for k,v in pairs(t) do
-        print(k.." : "..v)
-    end
+  for k,v in pairs(t) do
+    print(k.." : "..v)
+  end
 end
 
 function DBsearch(user_name,dbFile)
@@ -29,5 +46,66 @@ function DBsearch(user_name,dbFile)
 			return {user=user,salt=salt,hash=hash}
 		end
 	end
+end
+
+function shallow_copy(t)
+  local t2 = {}
+  for k,v in pairs(t) do
+    t2[k] = v
+  end
+  return t2
+end
+
+function serAng( x )
+  if (x<0) then x = 0; end;
+  if (x>100) then x = 100; end;
+  return servo.min+(servo.max-servo.min)*x/100;
+end
+
+function abre_porta()
+  local alarm_bzz=tmr.create()
+  local alarm_pwm=tmr.create()
+  -- starts sending servo PWM pulses
+  startPWM = function()
+    alarm_pwm:alarm(20, 1, 
+    function() 
+      gpio.write(pin,gpio.HIGH) tmr.delay(pl) gpio.write(pin,gpio.LOW)
+    end)
+  end
+  
+  closed_start_position = function()
+    pl = serAng(100) --commonly at 100% (maximum angle)
+    -- wait 5 seconds
+    alarm_bzz:alarm(5000,tmr.ALARM_SINGLE,
+    function() 
+      open_position()
+    end)  
+  end
+  
+  open_position = function()
+    pl = serAng(  0) --commonly at   0% (minimum angle)
+    -- wait 20 seconds
+    alarm_bzz:alarm(20000,tmr.ALARM_SINGLE,
+    function()
+      closed_end_position() 
+    end)
+  end
+
+  closed_end_position = function()
+    pl = serAng(100) --commonly at 100% (maximum angle)
+    -- wait 5 seconds
+    alarm_bzz:alarm(5000,tmr.ALARM_SINGLE,
+    function()
+      cleanup() 
+    end)    
+  end
+  
+  cleanup = function()
+    alarm_bzz:unregister()
+    alarm_pwm:unregister()    
+  end
+  
+  startPWM()
+  closed_start_position() 
 end
 
---
+++ main.lua
@@ -0,0 +1,135 @@
+
+function log(phrase)
+  if file.open("access.log","a") then
+    file.write(phrase.."\n")
+    file.close()
+  else
+    print("não foi possível registrar o log")
+  end
+end
+
+-- pl = serAng(100)
+
+set = wifi.setmode(wifi.STATION)
+wifi.sta.getap(1, listap)
+wifi.sta.disconnect()
+
+-- this part is only used the first time you need to connect to a network
+--station_cfg={}
+--station_cfg.ssid="HUAWEI-hNFT"
+--station_cfg.pwd= ""
+--station_cfg.auto=true
+--set = wifi.sta.config(station_cfg)
+
+wifi.sta.connect()
+print("config ",set)
+print(wifi.sta.status())
+
+local timer_count = 0;
+alarm_ip = tmr.create()
+alarm_ip:alarm(3000, 1,
+function()
+  if (wifi.sta.status() ~= wifi.STA_GOTIP ) then
+    print("no IP")
+  else
+    print(wifi.sta.getip())
+    alarm_ip:unregister()
+  end
+  timer_count = timer_count + 1
+  if (timer_count>20) then
+    alarm_ip:unregister()
+  end
+end)
+
+sntp.sync(nil, nil, nil, 1)
+
+pin_bot = 7
+time_repeat = 0;
+gpio.mode(7,gpio.INPUT,gpio.PULLUP)
+local alarm_porta = tmr.create():alarm(2000, tmr.ALARM_AUTO,
+function()
+  if time_repeat<1 then
+    if (gpio.read(pin_bot))==0 then
+      print("abre porta pelo botao")
+      time_repeat = 15
+      abre_porta()
+    end
+  else
+    time_repeat = time_repeat - 1
+  end
+end)
+
+function connection(conn)    
+--ARRUME ESSA FUNCAO, NAO COMPILA
+  print("   |||   \n")
+  local response_local = shallow_copy(response)
+  conn:on ("receive",
+  function(sck, req)
+    print(req)
+    local _, _, method, path_vars = string.find(req, "([A-Z]+) (.+) HTTP");
+    if(method=="POST")then
+      local _, _, user, pass = string.find(req, "user=(.+)&pass=(.+)");
+      --local req_tab = split(req,"\n") não existe essa função
+      print("peguei do post")
+      print(user)
+      print(pass)
+      --check if user and pass exist
+      local done_html = [[<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+</head>
+<link rel="icon" href="data:;base64,iVBORw0KGgo=">
+<body>
+  <title>Acesso - HackerSpace Maringá</title>
+  <meta name="description" content="Access page">
+  <meta name="author" content="HS Maringa">
+
+  
+  <H1>HackerSpace Maringá</H1><br>
+  ]]
+      sck:on("sent", function(sock) sock:close() end)
+      sck:send(done_html)
+      local r = DBsearch(user,"fakedb.csv")
+      if r ~= nil then
+        -- r.user,r.salt,r.hash
+        final_hash = crypto.toHex(crypto.hash("sha512",pass .. r.salt))
+        if (final_hash == r.hash) then
+          print("loggin allowed")
+          abre_porta()
+        else
+          print("loggin error, wrong password")
+        end
+      else
+        print("loggin error, no user")
+        return nil
+      end
+    elseif(method=="GET")then
+      print("é um get")
+      local function sender (sck)
+        if #response_local>0 then
+          sck:send(table.remove(response_local,1))
+        else
+          sck:close()
+          collectgarbage();
+        end
+      end
+      sck:on("sent", sender)
+      sender(sck)
+    else
+      print("é um outro")
+    end
+  end)  
+end
+
+srv=net.createServer(net.TCP)
+srv:listen(80, connection)
+print("all good")
+
+tmr.create():alarm(30000, tmr.ALARM_AUTO, function()
+if (wifi.sta.status() ~= wifi.STA_GOTIP) then
+  wifi.sta.connect()
+end
+end)
+
+
